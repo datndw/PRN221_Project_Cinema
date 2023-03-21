79350fe399650eb8a0917e5d8a8587b3ff50d8d1
Quang Hung <hungpham307@gmail.com>
2023-03-21 21:33:45 +0700

    load films and genres to home page

--- PRN221_Project_Cinema/Pages/Index.cshtml.cs
+++ PRN221_Project_Cinema/Pages/Index.cshtml.cs
@@ -1,20 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN221_Project_Cinema.Models;
 
 namespace PRN221_Project_Cinema.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly PRN221_Project_CinemaContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(PRN221_Project_CinemaContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+
+        [BindProperty]
+        public List<Movie> Movies { get; set; }
+
+        [ViewData]
+        public List<Genre> Genres { get; set; }
+        [FromQuery]
+        public string GenreId { get; set; }
 
         public void OnGet()
         {
+            Movies = _context.Movies.ToList();
+            Genres = _context.Genres.ToList();
 
+            if (GenreId != null)
+            {
+                Movies = _context.Movies
+                    .Where(m => m.GenreId == int.Parse(GenreId))
+                    .ToList();
+            }
+            else
+            {
+                Movies = _context.Movies.ToList();
+            }
         }
+        
     }
 }
