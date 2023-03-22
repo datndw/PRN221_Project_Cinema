0fe2daa2ade6d9662c812d5ae49c5391e57bb255
qnhanhh <caoquynhanh18@gmail.com>
2023-03-22 20:42:00 +0700

    add comment function

--- PRN221_Project_Cinema/Pages/Movies/Detail.cshtml.cs
+++ PRN221_Project_Cinema/Pages/Movies/Detail.cshtml.cs
@@ -17,22 +17,44 @@ namespace PRN221_Project_Cinema.Pages.Movies
         public Movie Movie { get; set; }
         public List<Rate> RateList { get; set; }
 
-        public IActionResult OnGet(int? id)
+        [BindProperty]
+        public Rate NewRate { get; set; }
+
+        [FromQuery(Name = "id")]
+        public int MovieId { get; set; }
+        public IActionResult OnGet()
         {
-            if (id == null)
+            if (MovieId == null)
             {
                 return NotFound();
             }
 
-            Movie = _context.Movies.Where(m => m.MovieId == id)
+            Movie = _context.Movies.Where(m => m.MovieId == MovieId)
                 .Include(m => m.Genre)
                 .FirstOrDefault();
 
-            RateList = _context.Rates.Where(r => r.MovieId == id)
+            RateList = _context.Rates.Where(r => r.MovieId == MovieId)
                 .Include(r => r.Person)
                 .ToList();
 
             return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["msg"] = "Please try again!";
+            }
+            else
+            {
+                NewRate.MovieId = 315162;
+                NewRate.PersonId = 7;
+                _context.Add(NewRate);
+                _context.SaveChanges();
+            }
+
+            return RedirectToPage("./Detail", new { id = MovieId.ToString() });
         }
     }
 }
