e38a9177a7c42f9c4e5cdc644e984737e3eff4ad
Quang Hung <hungpham307@gmail.com>
2023-03-23 22:40:19 +0700

    do pagination on home screen

--- PRN221_Project_Cinema/Pages/Index.cshtml.cs
+++ PRN221_Project_Cinema/Pages/Index.cshtml.cs
@@ -28,23 +28,39 @@ namespace PRN221_Project_Cinema.Pages
 
         public List<Rate> RateList { get; set; }
 
-        public void OnGet()
-        {
-            Genres = _context.Genres.ToList();
-            Movies = _context.Movies.Include(m => m.Genre).Include(m => m.Rates).ToList();
+        [BindProperty]
+        public int TotalPages { get; set; }
 
-            if (!string.IsNullOrEmpty(GenreId))
-            {
-                Movies = Movies
-                    .Where(m => m.GenreId == int.Parse(GenreId))
-                    .ToList();
-            }
+        [FromQuery(Name = "page")]
+        public int CurrentPage { get; set; }
+
+        public IActionResult OnGet(int page = 1)
+        {
+            Movies = _context.Movies.ToList();
+            const int pageSize = 6;
+           
+
+            int totalMovies = _context.Movies.Count();
+            // set the page to CurrentPage when user clicks another link
+            page = CurrentPage;
+
+            TotalPages = (int)Math.Ceiling(_context.Movies.Count() / 6.0);
+            Movies = Movies.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
             if (!string.IsNullOrEmpty(film))
             {
-                Movies = Movies.Where(m => m.Title.Contains(film))
-                    .ToList();
+                Movies = _context.Movies.Where(m => m.Title.Contains(film)).ToList();
             }
+            if (!string.IsNullOrEmpty(GenreId))
+            {
+                Movies = _context.Movies.Where(m => m.GenreId == int.Parse(GenreId)).ToList();
+            }
+            Genres = _context.Genres.ToList();
+            RateList = _context.Rates.ToList();
+
+            return Page();
         }
+       
 
     }
 }
